fd63274aeb0f155a6b8a55d534bf0b68a32f110e
jreyes0431 <jose-31.31@hotmail.com>
2025-02-19 23:16:50 -0500

    feat: saveVoiceNoteData method

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,5 +1,7 @@
 library services;
 
+import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:voice_app/models/models.dart';
 
 part 'user_repository.dart';
--- lib/services/user_repository.dart
+++ lib/services/user_repository.dart
@@ -6,4 +6,15 @@ class FirebaseRepository {
   Future<void> sendHelloWorld() async {
     await _firestore.collection('documents').add({'message': 'Hello World'});
   }
+
+  Future<void> saveVoiceNoteData(VoiceNote voiceNote) async {
+    try {
+      DocumentReference docRef = _firestore.collection('voiceNotes').doc();
+      voiceNote = voiceNote.copyWith(id: docRef.id);
+      await docRef.set(voiceNote.toJson());
+    } catch (e) {
+      log('Error saving voice note data: $e');
+      rethrow;
+    }
+  }
 }
